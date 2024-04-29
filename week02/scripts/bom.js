9d0924577ec8ae75a79c1c662a7c019a49860452
zwsut <zwsutherland@gmail.com>
2024-04-29 17:55:57 -0400

    w02 event handling learning page complete

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,26 +1,29 @@
-// 5
 const input = document.querySelector('#favchap');
-const button = document.querySelector('button')
+const button = document.getElementById('submit');
 const list = document.querySelector('#list');
-
-// 6
-const li = document.createElement('li');
-
-// 7
-const deleteButton = document.createElement('button');
-
-// 8
-li.textContent = input.value;
 
 // textContent is preferred over innerHTML because it is more secure.
 // However, if you need to include HTML tags, then you would use innerHTML.
 // textContent will not render HTML tags. It will display the tags as text.
 
-// 9
-deleteButton.textContent = '❌';
+button.addEventListener('click', function() {
+    if (input.value.trim() === '') {
+        input.setAttribute('placeholder', 'Please enter something.');
+        input.focus();
+    } else {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.value = '';
+        input.focus();
 
-// 10
-li.appendChild(deleteButton);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+});
 
-// 11
-list.appendChild(li);
